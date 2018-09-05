d014cd8ebb11667f193ab38d09d82cc96a839502
Davi Araujo Dal Fabbro <fabda01@gmail.com>
2018-09-04 22:57:45 -0300

    Make division consistent with PEP 238

--- ExtendedPCA/__init__.py
+++ ExtendedPCA/__init__.py
@@ -1,25 +1,26 @@
+import numpy as np
 from sklearn import decomposition
 
 class PCA(decomposition.PCA):
     def __init__(self,*args,**kwargs):
         decomposition.PCA.__init__(self,*args,**kwargs)
         
-    def fit_exp_var(self,X,exp_var_ratio,bounds=False):
+    def fit_exp_var(self,X,explained_variance,bounds=False):
         if not bounds:
             bounds = [1,X.shape[1]]
         
-        n_components = (bounds[0]+bounds[1])/2
+        n_components = (bounds[0]+bounds[1])//2
         
         while True:
             self.n_components = n_components
             self.fit(X)
             
-            if np.sum(self.explained_variance_ratio_)>exp_var_ratio:
+            if np.sum(self.explained_variance_ratio_) > explained_variance:
                 bounds[1] = n_components
-                n_components = (bounds[0]+bounds[1])/2
+                n_components = (bounds[0]+bounds[1])//2
             else:
                 bounds[0] = n_components
-                n_components = (bounds[0]+bounds[1])/2
+                n_components = (bounds[0]+bounds[1])//2
             
             if bounds[0] == bounds[1] or bounds[0] == bounds[1]-1:
                 self.n_components = n_components + 1
